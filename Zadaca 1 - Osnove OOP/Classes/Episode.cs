ac490af2128e8cc1cd82e9315fc5f50d3e687a34
AntonioCmrk <72660789+AntonioCmrk@users.noreply.github.com>
2020-10-25 21:34:10 +0100

    Zadaca 1 - Osnove OOP

---
+++ Zadaca 1 - Osnove OOP/Classes/Episode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes
+{
+    public class Episode
+    {
+        public int Viewers;
+        public double maxScore;
+        public double averageScore;
+        public Episode()
+        { 
+            Viewers = 0;
+            maxScore = 0;
+            averageScore = 0;
+        }
+        public Episode(int v, double mS, double aS)
+        {
+            Viewers = v;
+            maxScore = mS;
+            averageScore = aS;
+        }
+        public void AddView(double rS)
+        {
+            Viewers++;
+            if (maxScore < rS)  
+            {
+                maxScore = rS;
+            }
+        }
+        public double GetMaxScore() 
+        {
+            averageScore = averageScore + maxScore;
+            return maxScore;
+        }
+        public double GetAverageScore() 
+        {
+            return averageScore = averageScore / Viewers;
+        }
+        public double GetViewerCount() 
+        {
+            return Viewers;
+        }
+    }
+}
